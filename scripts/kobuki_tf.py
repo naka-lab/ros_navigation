7afc101a3ed80ca856d5f83b7810ae3d8c6c04d6
nakatomo <nakalab@example.com>
2022-02-27 17:23:42 +0900

    マップを2つ使用するlauch追加

--- scripts/kobuki_tf.py
+++ scripts/kobuki_tf.py
@@ -13,7 +13,7 @@ while not rospy.is_shutdown():
     br.sendTransform((0.0, 0.0, 0.01), (0, 0, 0, 1), rospy.Time.now(), 
         "base_link", "base_footprint")
 
-    br.sendTransform((0.0, 0.0, 0.24), (0, 0, 0, 1), rospy.Time.now(), 
+    br.sendTransform((0.14, 0.0, 0.24), (0, 0, 0, 1), rospy.Time.now(), 
         "base_scan", "base_link")
 
     r.sleep()
